c0a3fc6f02cf9c2795b481e9b721f0fd7e3aed46
Justin Hunter <justin.d.hunter@gmail.com>
2011-10-30 23:05:24 -0400

    basic session handling

--- lib/SO/Simple.pm
+++ lib/SO/Simple.pm
@@ -2,31 +2,70 @@ package SO::Simple;
 use Web::Simple 'SO::Simple';
 use HTML::Zoom;
 use Plack::Builder;
+use SO::Simple::Model;
 
 has template_dir => (
   is => 'rw',
+  lazy => 1,
   default => sub { $_[0]->config->{template_dir} }
+);
+
+has dsn => (
+  is => 'ro',
+  lazy => 1,
+  default => sub { $_[0]->config->{dsn} }
 );
 
 has zoom => (
   is => 'ro',
-  default => sub {
-    HTML::Zoom
-    ->from_file($_[0]->template_dir . '/index.html')
-    ->replace_content(title => $_[0]->config->{title})
-    ->replace_content('#a_home' => $_[0]->config->{title})
-  }
+  lazy => 1,
+  builder => '_build_zoom',
+  clearer => '_clear_zoom',
 );
+
+has session => (
+  is => 'rw',
+  lazy => 1,
+  default => sub { +{} },
+  clearer => '_clear_session',
+);
+
+has kioku => (
+  is => 'ro', 
+  lazy => 1,
+  builder => '_build_kioku',
+);
+
+sub _build_zoom {
+  my $self = shift;
+  my $session = $self->session;
+  my $rll = exists $session->{user} ?
+            '<li><a href="/logout">Logout</a></li>' :
+            '<li><a href="/login">Login</a></li>&nbsp;<li><a href="/register">Register</a></li>';
+
+  HTML::Zoom
+  ->from_file($self->template_dir . '/index.html')
+  ->replace_content(title => $self->config->{title})
+  ->replace_content('#a_home' => $self->config->{title})
+  ->replace_content('ul#register_login_logout' => \$rll);
+}
+
+sub _build_kioku {
+  my $self = shift;
+  my $k = SO::Simple::Model->new(dsn => $self->dsn,
+                                 extra_args => { create => 1 } ); 
+}
 
 sub default_config {
   ( title => 'SO::Simple',
-    template_dir => 'templates' );
+    template_dir => 'templates',
+    dsn => 'dbi:SQLite:so_simple.db' );
 }
 
 sub dispatch_request {
   sub (/) {
     my $self = shift;
-    my $content = 'STUFF HERE';
+    my $content = '';
     my $fh = $self->zoom
              ->replace_content('div#main' => $content)
              ->to_fh;
@@ -36,14 +75,39 @@ sub dispatch_request {
   sub (GET + /login) {
     my $self = shift;
     my @body;
-    HTML::Zoom->from_file($self->template_dir . '/login.html')->select('div#main')->collect_content({ into => \@body })->run;
+
+    HTML::Zoom
+    ->from_file($self->template_dir . '/login.html')
+    ->select('div#main')
+    ->collect_content({ into => \@body })
+    ->run;
+
     my $fh = $self->zoom->replace_content('div#main' => \@body)->to_fh; 
     [ 200, [ 'Content-type' => 'text/html' ], $fh ]
   },
 
   sub (POST + /login + %username=&password=) {
     my ($self, $username, $password) = @_;
-    [ 200, [ 'Content-type' => 'text/html' ], [ ] ]
+    my $env = $_[PSGI_ENV];
+    my $session = $env->{'psgix.session'};
+    my $dir = $self->kioku;
+    my $scope = $dir->new_scope;
+    my $user = $dir->lookup('user:' . $username)
+      or die 'Invalid username';
+    $user->check_password($password)
+      or die "Invalid password";
+    $session->{user} = $username;
+    $self->session($session);
+    $self->_clear_zoom;
+    [ 302, [ Location => '/' ], [] ];
+  },
+
+  sub (GET + /logout) {
+    my $self = shift;
+    delete $_[PSGI_ENV]->{'psgix.session'}{$_} for keys %{$_[PSGI_ENV]->{'psgix.session'}};
+    $self->_clear_session;
+    $self->_clear_zoom;
+    [ 302, [ Location => '/' ], [] ];
   }
 }
 
@@ -52,8 +116,8 @@ around to_psgi_app => sub {
   my $self = shift;
   my $app = $self->$orig(@_);
   builder {
-    enable 'Plack::Middleware::Static',
-      path => qr{^/(images|js|css)/}, root => 'static';
+    enable 'Static', path => qr{^/(images|js|css)/}, root => 'static';
+    enable 'Session';
     $app;
   };
 };
